5e91e8a5a78ad4102fe8237ffbc9c31999378f69
aldietrich <andrewldietrich@gmail.com>
2016-05-10 15:28:58 -0500

    Update
    Added date field, will console.log the result on submit

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'doSomething',
+      'click #buttonsubmit':'getTime',
+
     },
 
     doSomething: function() {
+    	this.switchTo('dateinput');
+    	console.log("doSomething ran!");
+
+    },
+
+    getTime: function() {
+    	var timeField = this.$('#datedata').val();
+    	console.log(timeField);
+    	console.log("getTime ran!");
     }
+
   };
 
 }());
